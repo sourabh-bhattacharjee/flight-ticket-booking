d8629fc07c3d606e59381e2bfa68136b61dd1e35
sourabhrockz <sourabh20051998@gmail.com>
2024-10-27 19:21:11 +0530

    second initial commit

--- src/component/MainBody.jsx
+++ src/component/MainBody.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function MainBody() {
     const style = {
         display: 'block',
         marginTop: '8px',
         marginBottom: '8px'
+    }
+    const [flightType,setFlightType] = useState("One way");
+    const [flightChng ,setFlightChng] = useState(false);
+    const [openDropDown,setOpenDropDown] = useState(false);
+    function changeFlightType(){
+        flightType === 'One way' ? setFlightType("Round Trip") : setFlightType('One way');
+        setFlightChng(!flightChng);
+    }
+    function changeopenDropDown(){
+        setOpenDropDown(!openDropDown);
     }
   return (
     <div className="main">
@@ -73,7 +83,28 @@ export default function MainBody() {
                         <div className="sec-main-booking">
                             {/* first line */}
                             <div className="firstLine">
-                                
+                                <div className="fr-sec" onClick={changeopenDropDown}>
+                                    {flightType === 'One way' ? <img className="frst-arr" src="https://cdn-icons-png.flaticon.com/128/664/664866.png" alt="arrow" /> : <img className="frst-arr" src="https://cdn-icons-png.flaticon.com/128/483/483339.png" alt="arrow" />}
+                                    <span className="one-way">{flightType}</span>
+                                    <span>
+                                        <img className="down-arr" src="https://cdn-icons-png.flaticon.com/128/2985/2985150.png" alt="down"/>
+                                    </span>
+                                    {openDropDown? 
+                                        <div className="fr-sec">
+                                            <div className="box-adj">
+                                                <li onClick= {changeFlightType} className="li-item">
+                                                    {flightType ==='One way'? 
+                                                    <img className= "leftIcon tickIcon" src="https://cdn-icons-png.flaticon.com/128/12836/12836714.png" alt="right"/>:<span style={{display:'inline-block'}} className= "leftIcon"></span>}
+                                                    <span style={{marginLeft: '12px'}} >One way</span>
+                                                </li>
+                                                <li onClick= {changeFlightType} className="li-item">
+                                                {flightType ==='Round Trip'? 
+                                                    <img className= "leftIcon tickIcon" src="https://cdn-icons-png.flaticon.com/128/12836/12836714.png" alt="right"/>:<span style={{display:'inline-block'}} className= "leftIcon"></span>}
+                                                   <span style={{marginLeft: '12px'}}>Round Trip</span> 
+                                                </li>
+                                            </div>
+                                        </div>:null}
+                                </div>
                             </div>
                         </div>
                     </div>
